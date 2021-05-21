ffdd802d382d1533ed1faa37a6fd13393b8ae5d5
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-05-22 00:00:56 +0530

    Update 11. Roof Top.java

--- Serach And Sort/gfg Self paced Searching/11. Roof Top.java
+++ Serach And Sort/gfg Self paced Searching/11. Roof Top.java
@@ -1,4 +1,16 @@
+You are given heights of consecutive buildings. You can move from the roof of a building to the roof of next adjacent building. You need to find the maximum number of consecutive steps you can put forward such that you gain an increase in altitude with each step.
 
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,2,3,2}
+Output: 1
+Explanation: 1, 2 or 2, 3 are the only consecutive 
+buildings with increasing heights.
+  
+  
+  
   class Solution
 {
     //Function to find maximum number of consecutive steps 
