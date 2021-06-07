535ed1dfd8414dc7a8cc603c80978a85eda93071
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2021-06-07 15:08:28 +0530

    Create 7. Check if strings are rotations of each other or not.java

---
+++ Strings/Gfg Self paced/7. Check if strings are rotations of each other or not.java
@@ -0,0 +1,31 @@
+Given two strings s1 and s2. The task is to check if s2 is a rotated version of the string s1. The characters in the strings are in lowercase.
+
+ 
+
+Example 1:
+
+Input:
+geeksforgeeks
+forgeeksgeeks
+Output: 
+1
+Explanation: s1 is geeksforgeeks, s2 is
+forgeeksgeeks. Clearly, s2 is a rotated
+version of s1 as s2 can be obtained by
+left-rotating s1 by 5 units.
+  
+  
+  
+  class Solution
+{
+    //Function to check if two strings are rotations of each other or not.
+    public static boolean areRotations(String s1, String s2 )
+    {
+        // Your code here
+        if(s1.length()!=s2.length())
+        return false;
+        return ((s1+s1).indexOf(s2)>=0);
+        //returns +ve for index else negative for not found
+    }
+    
+}//O(n)
